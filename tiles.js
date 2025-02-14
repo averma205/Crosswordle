0c234b676802a5f79d4cf2303dead78b5f75663b
averma205 <a5verma@ucsd.edu>
2025-02-14 03:15:08 -0800

    14 February 3:14

--- tiles.js
+++ tiles.js
@@ -1,1 +1,43 @@
+// crosswordContainer.style.gridTemplateColumns = "repeat(7, 40px)";
+const crosswordLayout = [
+    ['W', 'E', 'A', 'R', 'Y', ' ', ' '],
+    [' ', ' ', 'L', ' ', ' ', ' ', ' '],
+    [' ', ' ', 'I', 'N', 'G', 'O', 'T'],
+    [' ', ' ', 'E', ' ', 'U', ' ', ' '],
+    ['B', 'A', 'N', 'A', 'N', 'A', ' ']
+];
+
+const crosswordContainer = document.getElementById('crossword');
+
+function highlight(container, color) {
+    container.style.backgroundColor = color;
+}
+
+crosswordLayout.forEach(row => {
+    const crossRow = document.createElement('tr');
+    row.forEach(letter => {
+        const crossCol = document.createElement('td');
+        const num = document.createElement('label');
+        num.innerHTML = '0';
+        const cell = document.createElement('input');
+        cell.addEventListener("keyup", highlight(crossCol, '#b5b4b4'));
+        cell.addEventListener("keyup", highlight(crossCol, 'white'));
+        crossCol.appendChild(cell);
+        cell.type = 'text';
+        cell.maxLength = 1;
+        cell.classList.add('letter');
+        num.classList.add('number');
+        if (letter === ' ') {
+            cell.style.backgroundColor = 'black';
+            crossCol.style.backgroundColor = 'black';
+            cell.disabled = true;
+        }
+        else {
+            crossCol.appendChild(num);
+        }
+        crossRow.appendChild(crossCol);
+    });
+    crosswordContainer.appendChild(crossRow);
+    console.log(crossRow);
+});
 
